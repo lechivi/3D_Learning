0e69ff4723105a59f67990364802fe3bace91b7e
Asaka <lechivi39@gmail.com>
2023-05-09 17:22:47 +0700

    [any]

---
+++ Assets/_Data/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        movement = movement.normalized * moveSpeed;
+
+        GetComponent<Rigidbody>().AddForce(movement);
+    }
+}
