eb4901560faedaf7ad52886673346bce8b8615f3
MAY03\MAY03 <lechivi39@gmail.com>
2023-05-09 18:50:22 +0700

    [any[

--- Assets/_Data/Scripts/PlayerMovement.cs
+++ Assets/_Data/Scripts/PlayerMovement.cs
@@ -6,14 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
+    private float horizontalValue;
+    private float verticalValue;
+
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical);
-        movement = movement.normalized * moveSpeed;
+        //Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        ////movement = movement.normalized * moveSpeed;
 
-        GetComponent<Rigidbody>().AddForce(movement);
+        //GetComponent<Rigidbody>().AddForce(movement * moveSpeed);
+
+        this.horizontalValue = Input.GetAxis("Horizontal") * this.moveSpeed * Time.deltaTime;
+        this.verticalValue = Input.GetAxis("Vertical") * this.moveSpeed * Time.deltaTime;
+
+        transform.Translate(this.verticalValue, 0f, this.horizontalValue);
     }
 }
